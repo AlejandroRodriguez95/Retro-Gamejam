3eda74346ddbfa2a0d6c427a2b6bb7078210ad33
Mara <135391467+mara-13@users.noreply.github.com>
2023-06-19 10:50:38 -0400

    level background music

--- Assets/Project/Scripts/Audio/AudioManager.cs
+++ Assets/Project/Scripts/Audio/AudioManager.cs
@@ -3,6 +3,8 @@ using Sirenix.OdinInspector;
 
 public class AudioManager : MonoBehaviour
 {
+    [Title("SOUNDTRACK")]
+    [SerializeField] private AudioClip levelMusic;
     [Title("AUDIO CLIPS")]
     [SerializeField] [Tooltip("This sound will play when the ball goes off-screen, for now (we will change it later)")] AudioClip destructionClip;
     [SerializeField] AudioClip[] ballBounceClips;
@@ -11,10 +13,34 @@ public class AudioManager : MonoBehaviour
     [Title("Script references")]
     [SerializeField] BallLauncher ballLauncher;
     [SerializeField] BallDestroyer ballDestroyer;
+
+    private AudioSource source;
     private void Awake()
     {
         BallBehavior.OnBallCollidesWithObjectAUDIO += PlayRandomBounceClip;
         BallDestroyer.OnBallIsDestroyedAUDIO += PlayDestructionClip;
+        this.source = GetComponent<AudioSource>();
+    }
+
+    private void Start() {
+        this.PlaySoundrackByScene("level");
+    }
+
+    private void PlaySoundrackByScene(string scene)
+    {
+        if (this.source == null)
+        {
+            Debug.Log("Audio Source failed");
+            return;
+        }
+        switch (scene)
+        {
+            case "level":
+                this.source.clip = this.levelMusic;
+                break;
+        }
+        this.source.loop = true;
+        this.source.Play();
     }
 
     private void PlayDestructionClip(AudioSource audioSource)
